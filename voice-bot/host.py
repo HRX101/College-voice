0025baf6ed5cf9d6195a25b8da82b38e018c410a
Hrithik <74535054+HRX101@users.noreply.github.com>
2022-11-27 01:46:42 +0530

    Update host.py

--- voice-bot/host.py
+++ voice-bot/host.py
@@ -164,8 +164,9 @@ def app_sst(model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam
                 stream.feedAudioContent(buffer)
                 text = stream.intermediateDecode()
                 text_output.markdown(f"**Text:** {text}")
-                message=send_message(text_output)
-                st.write('bot: ',message)
+                if text_output="":
+                    message=send_message(text_output)
+                    st.write('bot: ',message)
                 #ref.child(name_of_user).push().set(question)
                 
         else:
